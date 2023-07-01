a3a78b44c1843ebc82c8aaf3b93c2fd37dd1023a
Kcyberh <101862000+Kcyberh@users.noreply.github.com>
2023-07-01 12:40:01 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -2,11 +2,25 @@ const section = document.querySelector(".content");
 const btn = document.querySelector("#start-btn");
 const email = document.querySelector("#email-box");
 const lbl = document.createElement("label");
-
+const img = document.createElement("img");
 function printValue(e){
     e.preventDefault();
     section.appendChild(lbl);
     alert(email.value);
     lbl.innerText = "Welcome, " + email.value;
-}
+    lbl.style.color = "red";
+    lbl.style.fontSize ="40px";
+    img.style.width = "300px"
+    img.src = "/images/3d.png"
+    section.appendChild(img);
+ }
+
+ function onHover() {
+img.style.width = "500px"
+ }
+ function onLeave(){
+    img.style.width = "300px"
+ }
 btn.addEventListener("click", printValue);
+img.addEventListener("mouseover", onHover);
+img.addEventListener("mouseout", onLeave);
