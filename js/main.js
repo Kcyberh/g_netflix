e22055d933e41608498c2d4e8d22918602c1209d
Kcyberh <101862000+Kcyberh@users.noreply.github.com>
2023-07-01 11:54:11 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-const btn = document.querySelectoer("#start-btn");
+const section = document.querySelector(".content");
+const btn = document.querySelector("#start-btn");
 const email = document.querySelector("#email-box");
-const lbl = document.querySelector("#lblStatus");
+const lbl = document.createElement("label");
 
-function printValue(){
+function printValue(e){
+    e.preventDefault();
+    section.appendChild(lbl);
     alert(email.value);
     lbl.innerText = "Welcome, " + email.value;
 }
+btn.addEventListener("click", printValue);
