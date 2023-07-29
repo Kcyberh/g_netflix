43a7a0423204d6f0b31b64b8425b2c253a2a24b9
Kcyberh <101862000+Kcyberh@users.noreply.github.com>
2023-07-29 15:35:52 +0100

    update

---
+++ js/detail.js
@@ -0,0 +1,44 @@
+// details
+function getMovieDetail(){
+    const options = {
+       method: 'GET',
+       headers: {
+         accept: 'application/json',
+         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjY0ZWZhYWFhZTNjYTVjOWI2ODhkYWQ4YzVmMmU4YiIsInN1YiI6IjYxNTI3M2E5ZDFjYTJhMDA0MjYxNGM2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XIg3w2HDHuUf6JBdCpUolGFOnsjYZltVN3hMeYrBppU'
+       }
+     };
+     
+     fetch('https://api.themoviedb.org/3/discover/movie', options)
+       .then(response => response.json())
+       .then(response => {
+          const results = response["results"];
+          for (let i = 0; i < 1; i++){
+            const button = document.createElement('button');
+             const div = document.createElement('div');
+             const h4 = document.createElement('h4');
+             const img = document.createElement('img');
+             const desc = document.createElement('p');
+             const yearText = document.createElement('p');
+             h4.innerText =results[i]["title"];
+             img.src = "https://image.tmdb.org/t/p/w500" + results[i]["poster_path"];
+             desc.innerText = results[i]["overview"];
+             yearText.innerText = results[i]["release_date"];
+             button.innerText ="View all";
+ 
+             div.appendChild(img);
+             div.appendChild(h4);
+             div.appendChild(desc);
+             div.appendChild(yearText);
+            div.appendChild(button);
+           
+             div.className = "card shadow border rounded m-4 p-4 w-25";
+             section.className ="d-flex flex-wrap justify-content-center";
+             section.appendChild(div);
+          }
+          console.log(response["results"])
+       }
+       )
+       .catch(err => console.error(err));
+             
+ }
+ getMovieDetail();
--- js/main.js
+++ js/main.js
@@ -99,4 +99,6 @@ function discoverMovies(){
       )
       .catch(err => console.error(err));
 }
+
+
 discoverMovies();
