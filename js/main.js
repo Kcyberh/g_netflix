9fe2bf300673d0041900ffc69e83d5f33c2d2b6a
Kcyberh <101862000+Kcyberh@users.noreply.github.com>
2023-07-29 11:55:11 +0100

    updated js file

--- js/main.js
+++ js/main.js
@@ -53,3 +53,40 @@ function loadData(){
    })
  }
 loadData();
+
+function discoverMovies(){
+   const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjY0ZWZhYWFhZTNjYTVjOWI2ODhkYWQ4YzVmMmU4YiIsInN1YiI6IjYxNTI3M2E5ZDFjYTJhMDA0MjYxNGM2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XIg3w2HDHuUf6JBdCpUolGFOnsjYZltVN3hMeYrBppU'
+      }
+    };
+    
+    fetch('https://api.themoviedb.org/3/discover/movie', options)
+      .then(response => response.json())
+      .then(response => {
+         const results = response["results"];
+         for (let i = 0; i < response.length; i++){
+            const div = document.createElement('div');
+            const h4 = document.createElement('h4');
+            const img = document.createElement('img');
+            const desc = document.createElement('p');
+            const yearText = document.createElement('p');
+            h4.innerText =results[i]["title"];
+            img.src = "https://imag.tmdb.org/t/p/w500" + results[i]["poster_path"];
+            desc.innerText = results[i]["overview"];
+            yearText.innerText = results[i]["release_date"];
+
+            div.appendChild(img);
+            div.appendChild(h4);
+            div.appendChild(desc);
+            div.appendChild(yearText);
+            section.appendChild(div);
+
+         }
+         console.log(response["results"])
+      }
+      )
+      .catch(err => console.error(err));
+}
